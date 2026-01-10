f04438da8bf48e6b1aa1194b8067cd8581622f8d
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-10 13:58:08 +0530

    efficient method

--- Logic Building/Basic Problem/check_even_or_odd.java
+++ Logic Building/Basic Problem/check_even_or_odd.java
@@ -1,7 +1,19 @@
 public class check_even_or_odd {
+    // public static boolean isEven(int n) {
+    // int rem = n % 2;
+    // if (rem == 0) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
+
+    // Best Case Scenario - The last bit of all odd numbers is always 1, while for
+    // even numbers it’s 0. So, when performing bitwise AND operation with 1, odd
+    // numbers give 1, and even numbers give 0.
+
     public static boolean isEven(int n) {
-        int rem = n % 2;
-        if (rem == 0) {
+        if ((n & 1) == 0) {
             return true;
         } else {
             return false;
