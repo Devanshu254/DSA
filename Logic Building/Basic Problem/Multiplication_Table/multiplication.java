053d16f8db7e9d6a53e04e1a4cdc63f4f493ea75
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-11 10:14:00 +0530

    added_public_static

--- Logic Building/Basic Problem/Multiplication_Table/multiplication.java
+++ Logic Building/Basic Problem/Multiplication_Table/multiplication.java
@@ -1,12 +1,26 @@
+// class multiplication {
+//     void printTable(int n) {
+//         for(int i=1;i<=10;i++) {
+//             System.out.println(n +"*"+ i + "=" + n*i);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         multiplication m = new multiplication();
+//         m.printTable(n);
+//     }
+// }
+
+
+// If we are not following OOP then we can simply make the method static, so that other methods can directly call it. Otherwise we will have to create an object.
 class multiplication {
-    void printTable(int n) {
+    public static void printTable(int n) {
         for(int i=1;i<=10;i++) {
             System.out.println(n +"*"+ i + "=" + n*i);
         }
     }
     public static void main(String[] args) {
         int n = 5;
-        multiplication m = new multiplication();
-        m.printTable(n);
+        printTable(n);
     }
 }
