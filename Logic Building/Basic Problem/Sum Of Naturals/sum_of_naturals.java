f10ffb73da20f5df5b30c671dda5b9c0a81339d1
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-11 14:33:15 +0530

    sc is closed

--- Logic Building/Basic Problem/Sum Of Naturals/sum_of_naturals.java
+++ Logic Building/Basic Problem/Sum Of Naturals/sum_of_naturals.java
@@ -1,12 +1,15 @@
 import java.util.*;
+
 public class sum_of_naturals {
     public static int sumOfNaturals(int n) {
-        return (n*(n+1))/2;
+        return (n * (n + 1)) / 2;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please provide input: ");
         int n = sc.nextInt();
+        sc.close();
         int res = sumOfNaturals(n);
         System.out.println(res);
     }
