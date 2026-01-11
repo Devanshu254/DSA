a92fa9d2c815dc62e978c7f83bb04d9b5ab16356
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-11 10:11:59 +0530

    solution1_normal

---
+++ Logic Building/Basic Problem/Multiplication_Table/multiplication.java
@@ -0,0 +1,12 @@
+class multiplication {
+    void printTable(int n) {
+        for(int i=1;i<=10;i++) {
+            System.out.println(n +"*"+ i + "=" + n*i);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        multiplication m = new multiplication();
+        m.printTable(n);
+    }
+}
