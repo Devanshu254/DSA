3b226a28a4da75f8c116507414358bc43d9ea3b9
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-11 10:24:08 +0530

    recursive_javascript_function

--- Logic Building/Basic Problem/Multiplication_Table/multiplication_table.js
+++ Logic Building/Basic Problem/Multiplication_Table/multiplication_table.js
@@ -1,7 +1,16 @@
-function printTable(n) {
-    for(let i=1;i<=10;i++) {
-        console.log(n +"*"+i+"="+ n*i);
-    }
+// function printTable(n) {
+//     for(let i=1;i<=10;i++) {
+//         console.log(n +"*"+i+"="+ n*i);
+//     }
+// }
+
+// recursive way to print the same function.
+function printTable(n, i = 1) {
+  // Base case
+  if (i == 11) return;
+  console.log(n + "*" + i + "=" + n * i);
+  i++;
+  printTable(n, i);
 }
 
 let n = 6;
