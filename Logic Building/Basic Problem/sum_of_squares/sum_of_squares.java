31752cafe2690da49ca6d39adbf9e57774bda279
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-11 15:59:17 +0530

    folder_name_changed due to package structure

--- Logic Building/Basic Problem/sum_of_squares/sum_of_squares.java
+++ Logic Building/Basic Problem/sum_of_squares/sum_of_squares.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class sum_of_squares {
     public static int sumOfSquares(int n) {
         int sum = 0;
@@ -9,7 +11,10 @@ class sum_of_squares {
         return sum;
     }
     public static void main(String[] args) {
-        int n = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the input: ");
+        int n = sc.nextInt();
+        sc.close();
         int result = sumOfSquares(n);
         System.out.print(result);
     }    
