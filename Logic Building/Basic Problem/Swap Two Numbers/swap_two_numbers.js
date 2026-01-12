e14eab177436dc57e10e3e28333a7648597c05ac
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-12 10:14:50 +0530

    javascript code

---
+++ Logic Building/Basic Problem/Swap Two Numbers/swap_two_numbers.js
@@ -0,0 +1,8 @@
+let a = 2, b = 3;
+console.log("Initial Value of A and B is ", "A: "+ a+ " B: "+b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("After swapping the value of A and B is ", "A: "+a+" B: "+b);
