900ad820d8229171e28b0804044a49e3b70c1532
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-13 20:47:23 +0530

    reading the input

--- Logic Building/Basic Problem/Closest Number/closest_number.java
+++ Logic Building/Basic Problem/Closest Number/closest_number.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class closest_number {
     static int closestNumber(int n, int m) {
         // Find the range.
         int closest = 0;
         int min_distance = Integer.MAX_VALUE;
-        for(int i=n-Math.abs(m); i<n+Math.abs(m);i++) {
-            if(i%m == 0) {
-                int dis = Math.abs(n-i);
-                if(dis < min_distance || dis == min_distance && Math.abs(i) > Math.abs(closest)) {
+        for (int i = n - Math.abs(m); i < n + Math.abs(m); i++) {
+            if (i % m == 0) {
+                int dis = Math.abs(n - i);
+                if (dis < min_distance || dis == min_distance && Math.abs(i) > Math.abs(closest)) {
                     closest = i;
                     min_distance = dis;
                 }
@@ -15,10 +17,14 @@ public class closest_number {
         // return condition.
         return closest;
     }
+
     public static void main(String[] args) {
-        int n = -15;
-        int m = 6;
-        int result = closestNumber(n,m);
-        System.out.println(result);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of m: ");
+        int m = sc.nextInt();
+        int result = closestNumber(n, m);
+        System.out.println("The closest number is: " + result);
     }
 }
