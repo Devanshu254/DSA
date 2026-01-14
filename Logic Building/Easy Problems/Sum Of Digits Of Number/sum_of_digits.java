d869fe3a15d266aacd5c23bed93740db574a84df
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-14 11:54:20 +0530

    adding input statement

--- Logic Building/Easy Problems/Sum Of Digits Of Number/sum_of_digits.java
+++ Logic Building/Easy Problems/Sum Of Digits Of Number/sum_of_digits.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class sum_of_digits {
     static int sumOfDigits(int n) {
         int sum = 0;
@@ -9,7 +11,9 @@ public class sum_of_digits {
         return sum;
     }
     public static void main(String args[]) {
-        int n = 657;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
         int result = sumOfDigits(n);
         System.out.print(result);
     }
