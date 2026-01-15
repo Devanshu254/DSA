f18a0d32768df72da9d9c9cd3b7d9b6619d0eab8
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-15 19:27:52 +0530

    added some other statement

--- Logic Building/Easy Problems/Prime Number/prime_number.java
+++ Logic Building/Easy Problems/Prime Number/prime_number.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class prime_number {
     static boolean primeNumber(int n) {
-        if(n<=1) return false;
-        for(int i=2;i<n;i++) {
-            if(n%i == 0) return false;
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
         }
         return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value: ");
         int n = sc.nextInt();
         System.out.print(primeNumber(n));
     }
