3dabad1cfc0e97a414259456cf40c7ecd66de87c
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-15 19:25:33 +0530

    prime number java code

---
+++ Logic Building/Easy Problems/Prime Number/prime_number.java
@@ -0,0 +1,13 @@
+public class prime_number {
+    static boolean primeNumber(int n) {
+        if(n<=1) return false;
+        for(int i=2;i<n;i++) {
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(primeNumber(n));
+    }
+}
