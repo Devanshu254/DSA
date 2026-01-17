344e47f3ff5760183a39fbbf0d90d78f7a7b7d7f
Devanshu Jha <devanshujha8305@gmail.com>
2026-01-17 20:08:17 +0530

    added the question statement

--- Logic Building/Easy Problems/Distance between 2 points/dis_between_two_points.java
+++ Logic Building/Easy Problems/Distance between 2 points/dis_between_two_points.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class dis_between_two_points {
     public static int disTwoPoints(int x1, int y1, int x2, int y2) {
         return (int) Math.round(
@@ -5,8 +7,15 @@ public class dis_between_two_points {
         );
     }
     public static void main(String args[]) {
-        int x1 = 0, x2 = 0;
-        int y1 = 2, y2 = -2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x1: ");
+        int x1 = sc.nextInt();
+        System.out.print("Enter the value of y1: ");
+        int y1 = sc.nextInt();
+        System.out.print("Enter the value of x2: ");
+        int x2 = sc.nextInt();
+        System.out.print("Enter the value of y2: ");
+        int y2 = sc.nextInt();
         int distance = disTwoPoints(x1, y1, x2, y2);
         System.out.print("The distance is: "+ distance);
     }
